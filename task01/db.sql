62d8d0662ee8bf54f949c522906362de95893141
Al3x4nd3 <109095032+Al3x4nd3@users.noreply.github.com>
2023-10-24 16:31:02 +0800

    test

--- task01/db.sql
+++ task01/db.sql
@@ -5,10 +5,12 @@ USE db;
 CREATE TABLE IF NOT EXISTS users (
     username VARCHAR(50) NOT NULL,
     email VARCHAR(100) NOT NULL,
-    password VARCHAR(50) NOT NULL
+    password VARCHAR(50) NOT NULL,
+    otp INT(6)
 );
 
 
 INSERT INTO users (username, email, password) VALUES
-('user', 'user@user.com', 'password')
+('user', 'user@user.com', 'password'),
+('test', 'latewe1340@ustorp.com', 'test');
 
---
+++ task01/login.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html>
+    <title>
+        Login Form
+    </title>
+    <head>
+        Login Form
+    </head>
+
+    <body>
+        <br>
+
+    
+
+        <form action="login.php" method="post">
+            <table>
+                <tr>
+                    <td><label for="username">Username:</label></td>
+                    <td><input type="text" id="username" name="username" required></td>
+                </tr>
+
+                <tr>
+                    <td><label for="password">Password:</label></td>
+                    <td><input type="text" id="password" name="password" required></br></td>
+                </tr>
+            </table>
+            <br>
+            <input type="submit" value="Submit">
+        </form>
+
+        <form action="forgot.php" method="post">
+            <button>Forgot Password</button>
+        </form>
+        
+    </body>
+</html>
+
+
+<?php  
+session_start();   //start of php
+
+
+//checks if http request is post (submit form)
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+    $host="localhost";
+    $username = "root";
+    $password = "";
+    $database = "db";
+
+    //connect to db, replace with actual username and password
+    $conn = new mysqli($host, $username, $password, $database);
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    #retrieves username and password from html login form
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    #find the user in db
+    $sql = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+    
+    #execute sql and store in result
+    $result = $conn -> query($sql);
+
+    if($result -> num_rows == 1){
+        header("location: welcome.php");
+    }
+
+    else
+        #$login_error = "Login failed. Please enter the correct login credentials.";
+
+        $alertMessage = "Login failed. Please enter the correct login credentials.";
+        echo "<script>alert('$alertMessage');</script>";
+
+    $conn->close();
+}
+
+?>
