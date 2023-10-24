2a078608681bb9b5bd69c5ca5b9f0e648b4143bd
jiasyuen <jiasyuen1426@1utar.my>
2023-10-24 16:32:22 +0800

    delete

--- task01/config/db_connect.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    //connect to database
-    $conn = mysqli_connect('localhost','root','','db');
-
-    //check connection
-    if(!$conn){
-        echo 'Connection error: ' . mysqli_connect_error();
-    }
-?>
