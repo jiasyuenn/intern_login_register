2a7e5e76a3fee0e03466c92b7795ab794f3aa364
jiasyuen <jiasyuen1426@1utar.my>
2023-10-25 10:46:51 +0800

    db_modified01

--- task01/db.sql
+++ task01/db.sql
@@ -3,14 +3,18 @@ CREATE DATABASE IF NOT EXISTS db;
 USE db;
 
 CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
     email VARCHAR(100) NOT NULL,
     password VARCHAR(50) NOT NULL,
-    otp INT(6)
+    otp INT(6) DEFAULT NULL,
+    status VARCHAR(50) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+
 );
 
 
-INSERT INTO users (username, email, password) VALUES
-('user', 'user@user.com', 'password'),
-('test', 'latewe1340@ustorp.com', 'test');
+INSERT INTO users (username, email, password, otp, status, created_at) VALUES
+('user', 'user@user.com', 'password',NULL,'unverified',CURRENT_TIMESTAMP),
+('test', 'latewe1340@ustorp.com', 'test',NULL,'unverified',CURRENT_TIMESTAMP);
 
